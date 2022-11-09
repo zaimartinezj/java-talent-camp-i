60dd09ad0e2a716d6e8d85c505fc10ec4c475f04
zaimartinezj <zaimartinezjarse@gmail.com>
2022-11-09 10:05:27 -0300

    agrego ejercicios

---
+++ ejercicio-hello-springmvc/src/main/java/carmelo/spring/webmvc/HelloController.java
@@ -0,0 +1,20 @@
+package carmelo.spring.webmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	// http://localhost:8080/poc-spring-web-mvc/hello
+	@RequestMapping(value = "/hello")
+	public ModelAndView saluda() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("titulo", "Tutoriales Spring MVC");
+		mv.addObject("mensaje", "Mi Primera Aplicacion Web Spring MVC");
+		mv.setViewName("hello");
+
+		return mv;
+	}
+}
